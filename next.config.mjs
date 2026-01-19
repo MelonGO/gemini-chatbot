0462b9eb4fe0ebaab2c03464e22ca35ad3935411
MelonGO <494216469@qq.com>
2026-01-20 03:15:31 +0800

    update next.config.mjs to increase server action body size limit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    }
+  },
   images: {
     remotePatterns: [],
   },
